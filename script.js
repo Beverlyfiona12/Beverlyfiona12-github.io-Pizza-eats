d8f8769d09cff19c66d3b075d2dcc369e49ae2ef
Beverlyfiona12 <awuorrae@gmail.com>
2022-03-20 15:25:52 +0300

    Delivery function

--- script.js
+++ script.js
@@ -379,8 +379,7 @@ $(function () {
         let newOrder = order(flavour, size, crust, topping, number, totalPrice);
         console.log(newOrder); // test func
 
-        //create a new object
-        // let myOrder = JSON.stringify(JSON.parse(newOrder));
+       
 
         //Write to the order
         $('.summary').slideDown(2000);
@@ -398,7 +397,26 @@ $(function () {
             + newOrder.total + "<br><br>").css('font-family', 'system-ui').css('font-size', '24px');
     });
 
-    
+    //Deliver
+    $(".deliver").click(function () {
+        $('.summary').slideUp();
+        $('#list').slideUp();
+        $('.summary').text("Provide location details").slideDown();
+        $('.deliver').hide(1000);
+        $('.delivernot').hide(1000);
+        $('.cdata-overlay').slideDown();
+    });
+
+
+    //Pick Up
+    $(".delivernot").click(function () {
+
+    });
+
+    //Scrollify
+    $(function () {
+        $.scrollify.move('#sum-order');
+
         
     });
 });
